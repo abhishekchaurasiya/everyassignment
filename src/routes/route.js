d262189bc0963ca05ded05d1c90bac4f2d1aa626
abhishekchaurasiya <abhichek2580@gmail.com>
2022-04-08 18:01:53 +0530

    hello

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,174 +1,51 @@
 const express = require('express');
+const loggerModule = require('../logger/logger')
+const helperModule = require('../util/helper')
+const formatterModule = require('../validator/formatter')
+const lodash = require('lodash')
 
-let _ = require("lodash")
 const router = express.Router();
 
+router.get('/test-me', function (req, res) {
+    loggerModule.welcomeMessage()
+    helperModule.printTodaysDate()
+    helperModule.printCurrentMonth()
+    helperModule.printBatchInformation()
+    formatterModule.trimString()
+    formatterModule.changeCaseToUpper()
+    formatterModule.changeCaseToLower()
+    res.send('My first ever api!')
+});
 
+router.get('/hello', function (req, res) {
+    // Problem a)
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    let subArrays = lodash.chunk(months, 3)
+    console.log('The result after splitting the months array is ', subArrays)
 
-router.get('/movies', function (req, res) {
+    // Problem b)
 
-    // solution no 1
-    let moviename = ['rang de basanti', 'the shining', 'lord of the rings', 'batman begin', 'kgf chapater one'];
+    let oddNumbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+    console.log('The last 9 odd numbers in the array are: ', lodash.tail(oddNumbers))
 
-    res.send(moviename)
+    // Problem c)
+    let a = [1, 2, 1, 4]
+    let b = [2, 3, 4, 3]
+    let c = [6, 1, 5, 10]
+    let d = [1, 1, 1]
+    let e = [1, 2, 3, 4, 5]
 
+    console.log('Final array or unique numbers is : ', lodash.union(a, b, c, d, e))
+
+    // Problem d)
+    let arrayOfKeyValuePairs = [["horror", "The Shining"], ["drama", "Titanic"], ["thriller", "Shutter Island"], ["fantasy", "Pans Labyrinth"]]
+    console.log('The object created from arrays is :', lodash.fromPairs(arrayOfKeyValuePairs))
+    res.send('My hello api!')
 });
 
 
-router.get('/movies/:indexNumber', function (req, res) {
-
-    // GET /movies/1 is a valid request and it should return the movie in your array at index 1 
-    // problem 2
-
-    let movieName = ['rang de basanti', 'the shining', 'lord of the rings', 'batman begin', 'kgf chapater one', "border", "solay", "dewar"];
-
-    //     let i = req.params.indexNumber;
-    //     let showMovieName;
-    //     for (j = 1; j <= movieName.length; j++) {
-    //         let num = movieName[i - 1];
-    //         showMovieName = num
-    //     }
-    //     res.send(showMovieName)
-
-    // Handle a scenario in problem 2 where if the index is greater than the valid maximum value a message is returned that tells the user to use a valid index in an error message.
-    // solution no 3
-    // Put in validation
-    let i = req.params.indexNumber;
-    let showMovieName;
-    for (j = 1; j <= movieName.length; j++) {
-        let num = movieName[i - 1]; 
-        if (num >= i) {
-            showMovieName = num;
-        } else {
-            showMovieName = "Please enter the valid movie number"
-        }
-    }
-    let result = showMovieName.charAt(0).toUpperCase() + showMovieName.slice(1).toLowerCase()
-    res.send(result)
-});
 
 
-router.get('/films', function (req, res) {
-
-    // solution no 4
-    let movieArrName = [
-        {
-            id: 1,
-            name: 'The Shining'
-        },
-        {
-            id: 2,
-            name: 'Incendies'
-        },
-        {
-            id: 3,
-            name: 'Rang de Basanti'
-        },
-        {
-            id: 4,
-            name: 'Finding Nemo'
-        }
-    ]
-
-    res.send(movieArrName)
-
-});
-
-router.get('/films/:filmId', function (req, res) {
-    // solution no 5
-    let movieArrName = [
-        {
-            id: 1,
-            name: 'The Shining'
-        },
-        {
-            id: 2,
-            name: 'Incendies'
-        },
-        {
-            id: 3,
-            name: 'Rang de Basanti'
-        },
-        {
-            id: 4,
-            name: 'Finding Nemo'
-        }
-    ]
-
-    let i = req.params.filmId;
-    let movieArrObject;
-    for (x = 1; x < movieArrName.length; x++) {
-        let indexValue = movieArrName[i - 1];
-        if (indexValue >= i) {
-            movieArrObject = indexValue;
-        } else {
-            movieArrObject = ` No movies exists with this id:- ${i}`
-        }
-    }
-    res.send(movieArrObject)
-    // res.send(movieArrObject.name)
-    // res.send(Object.entries(movieArrObject))
-
-});
-
-
-router.get("/array", function (req, res) {
-
-    // let numbers = [0, 1, 2, 3, 4, 5, 7, 8, 9];
-    // let missing = [];
-
-    // // Find the missing array items
-    // for (let i = 0; i < numbers.length; i++) {
-
-    //     if ((numbers[i + 1] - numbers[i]) > 1) {
-    //         missing.push(numbers[i + 1] - numbers[1]);
-    //     }
-    // }
-
-
-    // let numbers = [1, 3, 4, 5, 7, 8, 9]; // Missing 2,6
-    // let expectedNumber = numbers.length + 1;
-    // let totalSumNumberr = expectedNumber * (expectedNumber + 1) / 2;
-    // let sum = 0;
-    // for (i = 0; i < numbers.length; i++) {
-    //     sum = numbers[i];
-    // }
-    // let newVal = (totalSumNumberr - sum);
-    // console.log(newVal);
-
-    // function missing(arr) {
-    //     let missArry = [];
-
-    //     // Math.min(1,2,3,4)  //1
-    //     let minArry = Math.min(...arr)
-    //     let maxArr = Math.max(...arr)
-
-    //     // indexOf return its position, not there in -1
-    //     for (let x = minArry; x < maxArr; x++) {
-    //         if (arr.indexOf(x) < 0) {
-    //             missArry.push(x)
-    //         }
-    //     }
-    //     return missArry;
-    // }
-
-    // // let numbers = [1, 2, 3, 4, 5, 7, 8, 9, 12, 15]
-    // let numbers = [33, 34, 35, 37, 38, 39]
-    // let result = missing(numbers)
-
-    let numbers = [1, 2, 3, 4, 5, 7, 8, 9];
-    let n = numbers.length + 1;
-    let sumOfTheArray = (n * (n + 1)) / 2;
-
-    for (let index = 0; index < numbers.length; index++) {
-        sumOfTheArray = sumOfTheArray - numbers[index];
-    }
-    let valuse = sumOfTheArray;
-    console.log(valuse);
-    res.send("hello")
-
-
-});
 
 module.exports = router;
 // adding this comment for no reason
