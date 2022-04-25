8ee0eac5bf72bc8c0518d7b39601bec76c721254
abhishekchaurasiya <abhichek2580@gmail.com>
2022-04-25 12:30:54 +0530

    ok

--- src/controllers/randomController.js
+++ src/controllers/randomController.js
@@ -1,9 +1,72 @@
-let addToArray= function (req, res) {
-    let x= req.body.number
-    console.log(x)
-    let arr= [2, 5, 11, 14]
-    arr.push(x)
-    res.send( {  msg: "post req 3", data: arr  } )
+const bookUserModel = require('../models/bookmodelschema');
+
+const createBookUserData = async (req, res) => {
+    let data = req.body;
+    let userData = await bookUserModel.create(data);
+    res.send({ message: userData })
 }
 
-module.exports.addToArray= addToArray
+
+const getBookInfo = async (req, res) => {
+    let userData = await bookUserModel.find().select({ bookName: 1, authorName: 1, _id: 0 })
+    res.send({ message: userData })
+}
+
+const getBooksInYear = async (req, res) => {
+    let data = req.body
+    data.year
+    let allbooks = await bookUserModel.find(data)
+    res.send({ message: allbooks })
+}
+
+const getParticularBooks = async (req, res) => {
+    let data = req.body;
+    let allBooks = await bookUserModel.find(data)
+    res.send({ message: allBooks })
+}
+
+const priceDetails = async (req, res) => {
+    let data = req.body;
+    data.year;
+    let allBooks = await bookUserModel.find({ "price.indian": { $in: ["100INR", "200INR", "500INR"] } })
+    res.send({ message: allBooks })
+}
+
+// hence the condition will differ based on what you input in the request body
+// const getAllThings = async (req, res) => {
+//     let data = req.body;
+//     data.bookName;
+//     data.year;
+//     data.authorName;
+//     data.price.indianPrice;
+//     data.totalPage;
+
+
+//     //     const getParticularBooks = async function(req,res)
+//     // {
+//     //   let condition = req.body
+//     //   let particularBooks = await bookModel.find(condition)
+
+//     //   res.send(particularBooks)
+//     // }
+
+//     let allBooks = await bookUserModel.find(data).select({ bookName: true, authorName: true, price: { indianPrice: true }, _id: 0 }).select({ bookName: true, year: true, authorName: true, price: { indianPrice: true }, totalPage: true, _id: 0 })
+//     res.send({ message: allBooks })
+// }
+
+const getRandomBooks = async (req, res) => {
+    let randomBook = await bookUserModel.find({ $or: [{ stcokAvailable: true }, { totalPage: { $gt: 500 } }] })
+    // .select({ totalPage: true, bookName: true, authorName: true, price: { indianPrice: true }, _id: 0 })
+    res.send({ msg: randomBook })
+
+}
+
+
+module.exports.createBookUserData = createBookUserData;
+module.exports.getBookInfo = getBookInfo
+module.exports.getBooksInYear = getBooksInYear;
+module.exports.getParticularBooks = getParticularBooks
+module.exports.priceDetails = priceDetails
+module.exports.getXINRBooks = getXINRBooks
+module.exports.getRandomBooks = getRandomBooks
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,69 +2,53 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    // let a = { msg: "My first ever API response in JSON !!"} 
+let players = [
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ],
+    },
 
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ],
+    },
+]
 
-    res.send( { msg: "My first ever API response in JSON !!"} )
-});
+router.post("/players", function (req, res) {
 
+    let allData = req.body
+    let returnData = 0;
+    players.filter((x) => {
+        if (x.name === allData.name) {
+            returnData = 1;
+        }
+    })
+    if (returnData === 1) {
+        return res.send({ message: "This name is already exists" });
+    } else {
+        players.push(allData)
+        console.log(players);
 
+    }
+    // res.send(players
 
-router.get('/test-api1', function (req, res) {
+    res.send({ data: players, status: true })
 
-    res.send( "hi FunctionUp " )
-});
+    // res.send('hello')
+})
 
-
-router.get('/test-api2', function (req, res) {
-
-    res.send( { msg: "Hi FUnctionUp..again !"} )
-});
-
-
-router.get('/test-api3', function (req, res) {
-
-    res.send( { msg: "Hi FUnctionUp..again..this is another similar api !"} )
-});
-
-
-router.get('/test-api4', function (req, res) {
-
-    res.send( { msg: "Hi FUnctionUp..again..this is another similar api ..not I am getting bored!"} )
-});
-
-
-router.get('/test-api5', function (req, res) {
-
-    res.send( { msg: "Hi FUnctionUp" , name:"FunctionUp", age: "100"} )
-});
-
-
-
-router.get('/test-api6', function (req, res) {
-
-    res.send( {   data: [12, 24, 36, 48, 60]  }   )
-});
-
-router.post('/test-post1', function (req, res) {
-
-    res.send( {  msg: "hi guys"  }   )
-});
-
-
-// to send data in  post request-> prefer sending in BODY -> click body-raw-json
-router.post('/test-post2', function (req, res) {
-    let data= req.body
-    console.log(data)
-    res.send( {  msg: "hi guys..my 2nd post req"  }   )
-});
-
-
-const randomController= require("../controllers/randomController.js")
-//write a post request to accept an element in post request body and add it to the given array and return the new array
-router.post('/test-post3', randomController.addToArray ); //HANDLER/CONTROLLER
 
 
 
 module.exports = router;
+// adding this comment for no reason
